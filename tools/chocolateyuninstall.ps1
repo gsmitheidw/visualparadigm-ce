7388d36676886eaba90f806c6cf154d3a1677464
Graham Smith <gsmitheidw@gmail.com>
2021-06-30 15:39:28 +0100

    improved uninstaller

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,1 +1,27 @@
-#uninstall.exe -q
+$ErrorActionPreference = 'Stop';
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'visualparadigm-ce*'
+
+$packageName='visualparadigm-ce'
+
+if ($key.Count -eq 1) {
+
+  $key | ForEach-Object {
+  $packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'EXE'
+    silentArgs     = '-q'
+    validExitCodes = @(0)
+    file           = "C:\Program Files\Visual Paradigm CE 16.3\uninstaller\uninstall.exe"
+  }
+ Uninstall-ChocolateyPackage @packageArgs
+ }
+
+}
+else {
+Write-Warning "$key.Count matches found, aborting multiple copy uninstall!"
+
+}
+
+
+
